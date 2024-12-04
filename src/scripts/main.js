e1e5a4d13e8043e47cc90b1553ce44d9526fb284
Gonca <smggonca@gmail.com>
2024-12-04 12:39:07 -0300

    First Commit

---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+
+function changeTheme(){
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    if (currentTheme == "dark") rootHtml.setAttribute("data-theme", "light")
+    else rootHtml.setAttribute("data-theme", "dark")
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+
+toggleTheme.addEventListener("click", changeTheme);
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach(item =>{
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
